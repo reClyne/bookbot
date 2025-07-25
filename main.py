d6e9fdf6dacc14d1b0caae5525e88baf26165bd3
rclyne <rbclyne@gmail.com>
2025-07-25 18:42:32 -0400

    Finished bookbot

--- main.py
+++ main.py
@@ -9,15 +9,25 @@ def get_book_text(filepath):
         file_contents = f.read()
         return file_contents
 
+def no_book_path():
+    n_list = sys.argv
+    if len(n_list) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+def get_book():
+    n_list = sys.argv
+    return n_list[1]
 
 
 def main():
-    sText = get_book_text("books/frankenstein.txt")
+    no_book_path()
+    sText = get_book_text(get_book())
     num_words = wordcount(sText)
     # print( num_words, "words found in the document")
     s_list = sort_dict(letter_count(sText))
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print("Analyzing book found at ",get_book(),"...", sep="")
     print("----------- Word Count ----------")
     print("Found", num_words, "total words")
     print("--------- Character Count -------")
@@ -28,7 +38,7 @@ def main():
             print(str,":", " ", n, sep="")
     print("============= END ===============")
 
-    print(sys.argv)
+    #print(sys.argv)
 
 main()
     
